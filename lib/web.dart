2f15efbda0a430cc90bd003c49d98c1c942b7ffe
keidroid <kei.hasegawa@gmail.com>
2019-05-11 10:48:11 +0900

    位置調整

--- lib/web.dart
+++ lib/web.dart
@@ -11,7 +11,7 @@ class LightsOut extends RenderBox {
   LightsOut();
 
   static const W = 720.0/2, H = 720.0/2, P = 120.0/2, Q = 140.0/2, X = 20.0/2, Y = 60.0/2;
-  static const FL = 128.0/2, FM = 96.0/2, FS = 64.0/2;
+  static const FL = 128.0/2, FM = 96.0/2, FS = 64.0/2, FSS = 24.0;
 
   List<List<int>> panels;
   List<int> result = List.filled(5 * 5, 0);
@@ -58,7 +58,7 @@ class LightsOut extends RenderBox {
     c.translate(0, contentY);
 
     c.save();
-    c.translate(360/2, 360/2);
+    c.translate(360/2, 360/2 + 20);
     c.rotate(rot);
     c.drawOval(Rect.fromLTRB(-320.0/2, -300.0/2, 320.0/2, 300.0/2), bg);
     c.drawOval(Rect.fromLTRB(-300.0/2, -320.0/2, 300.0/2, 320.0/2), bg);
@@ -68,34 +68,35 @@ class LightsOut extends RenderBox {
       for (int x = 0; x < 5; x++) {
 
         c.drawRect(Rect.fromLTWH(X + Q * x, Y + Q * y, P, P), panels[y][x] > 0 ? on : off);
-/* crash: drawRRect 
+// crash: drawRRect
+/*
         c.drawRRect(
-            RRect.fromRectXY(Rect.fromLTWH(X + Q * x, Y + Q * y, P, P), 8, 8),
-            panels[y][x] > 0 ? on : off);
+  RRect.fromRectXY(Rect.fromLTWH(X + Q * x, Y + Q * y, P, P), 8, 8),
+  panels[y][x] > 0 ? on : off);
 */
         if (state == 0) {
-          text(c, '${y * 5 + x + 1}', FS, X + 24/2 + Q * x, Y + 24/2 + Q * y,
-              result[y * 5 + x] == 0);
+text(c, '${y * 5 + x + 1}', FS, X + 24/2 + Q * x, Y + 24/2 + Q * y,
+    result[y * 5 + x] == 0);
         }
       }
     }
 
     if (state == 0) {
-      text(c, 'Lights Out', FL, 80/2, -100/2);
+      text(c, 'Lights Out', FL, 80/2, -120/2);
       if (!result.contains(0)) {
-        text(c, 'Thank you for playing!!', FS, 48/2, 980/2);
+        text(c, 'Thank you for playing!!', FS, 48/2, 374.0);
       }
     } else {
-      text(c, 'Stage', FS, 40/2, -64/2);
-      text(c, '$stage', FM, 220/2, -96/2);
-      text(c, 'Step', FS, 320/2, -66/2);
-      text(c, '$step / $clear', FM, 460/2, -96/2);
+      text(c, 'Stage', FSS, 16, -60);
+      text(c, '$stage', FM, 16, -FM + 10);
+      text(c, 'Step', FSS, 160, -60);
+      text(c, '$step / $clear', FM, 160, -FM + 10);
     }
     if (state == 3) {
-      text(c, 'Success!', FM, 160/2, 220.0/2 + diff * diff);
+      text(c, 'Success!', FM, 160/2, 370.0 + diff * diff);
     }
     if (state == 4) {
-      text(c, 'Failed..', FM, 220/2, 220.0/2 + diff * diff);
+      text(c, 'Failed..', FM, 220/2, 370.0 + diff * diff);
     }
     c.restore();
   }
@@ -154,11 +155,36 @@ class LightsOut extends RenderBox {
 
     //json stage data
     var s = [
-      [12],[11,13],[2,10,12],[21,22,23],[7,12,17,22],
-      [12],[11,13],[2,10,12],[21,22,23],[7,12,17,22], //sample
-      [12],[11,13],[2,10,12],[21,22,23],[7,12,17,22], //sample
-      [12],[11,13],[2,10,12],[21,22,23],[7,12,17,22], //sample
-      [12],[11,13],[2,10,12],[21,22,23],[7,12,17,22], //sample
+      //01 - 05
+      [12],
+      [11,13],
+      [2,10,12],
+      [21,22,23],
+      [7,12,17,22],
+      //06 - 10
+      [5,8,15,18],
+      [15,17,19,20,22,24],
+      [0,2,4,20,22,24],
+      [0,5,10,18,23],
+      [2,8,12,17,22],
+      //11 - 15
+      [0,4,12,20,24],
+      [2,5,16,19,22],
+      [0,5,6,11,12,17],
+      [0,5,10,11],
+      [1,2,3,13,14,15,20,24],
+      //16 - 20
+      [0,11,13,17,24],
+      [3,8,9,13,15,16,17,21],
+      [11,13,15,17,19,20,22,24],
+      [5,9,10,11,13,14,15,19],
+      [0,2,4,10,12,14,20,22],
+      //21 - 25
+      [2,6,7,8,10,11,13,14,16,17,18,22],
+      [0,1,2,6,8,11,14,15,16,17,18,19,21,24],
+      [0,1,2,5,6,8,9,10,12,13,16,17,18,21,24],
+      [0,1,3,5,6,7,18,20,22,23,24],
+      [0,1,5,6,8,9,12,13,14,16,17,18,21,22,24],
     ];
     clear = 0;
     for (int p in s[stage - 1]) {
@@ -188,9 +214,9 @@ class LightsOut extends RenderBox {
       [bool on = true]) {
     var builder = ParagraphBuilder(ParagraphStyle())
       ..pushStyle(ui.TextStyle(
-          fontFamily: 'Roboto-Thin',
-          color: Color(on ? 0xddffffff : 0x44ffffff),
-          fontSize: size))
+fontFamily: 'Roboto-Thin',
+color: Color(on ? 0xddffffff : 0x44ffffff),
+fontSize: size))
       ..addText(text);
     c.drawParagraph(
         builder.build()..layout(ParagraphConstraints(width: W)), Offset(x, y));
@@ -212,16 +238,13 @@ class LightsOut extends RenderBox {
 
   get bg => Paint()
     ..color = Color(0x10ffffff);
-//    ..maskFilter = solid(2);
 
   get on => Paint()
-    ..color = Color(0xccffffff);
-//    ..maskFilter = solid(20);
+    ..color = Color(0xaaffffff);
 
   get off => Paint()
-    ..color = Color(0xccffffff)
+    ..color = Color(0x88ffffff)
     ..style = PaintingStyle.stroke;
-//    ..maskFilter = solid(2);
 }
 
 void main() {
